b259674edc5e8fd4982b687d2e3e84020fedcdfc
maheshwaran <152316758+MaheshWaran-B@users.noreply.github.com>
2026-09-16 09:33:25 +0530

    Add Reversepyramid class for pattern printing
    Implement Reverse Pyramid pattern printing in Java.

--- Pattern/Reversepyramid.java
+++ Pattern/Reversepyramid.java
@@ -1,1 +1,16 @@
+package Pattern;
+
+public class Reversepyramid {
+    public static void main(String[]args){
+        int n = 6;
+        for (int i =n;i>=1;i--){
+
+            System.out.println();
+
+            for(int j=1;j<=i;j++)
+                
+                System.out.print("*"+" ");
+        }
+    }
+}
 
