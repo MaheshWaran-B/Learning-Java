23391f43dd152633b1985b9bc1d1072503194436
maheshwaran <152316758+MaheshWaran-B@users.noreply.github.com>
2026-09-16 09:27:41 +0530

    Remove Sumofdigit class implementation

--- Loops/Sumofdigit.java
+++ Loops/Sumofdigit.java
@@ -1,16 +1,1 @@
-package Loops;
 
-public class Sumofdigit {
-    public static void main(String[] args) {
-        int num = 12345;
-        int sum = 0;
-        
-        while (num > 0) {
-            int digit = num % 10;
-            sum = sum + digit;    
-            num = num / 10;       
-        }
-        
-        System.out.println("Sum of digits: " + sum);
-    }
-}
