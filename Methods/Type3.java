8030364ec9bca0734e2e0a0534fb3f942a3ecf64
maheshwaran <152316758+MaheshWaran-B@users.noreply.github.com>
2026-09-16 09:29:43 +0530

    Remove Type3 class and its methods

--- Methods/Type3.java
+++ Methods/Type3.java
@@ -1,15 +1,1 @@
-package Methods;
-// without return type && with arguments
-public class Type3 {
-    public void addvalue(int a, int b){
-        int sum = a+b;
-    System.out.println(sum);
-    }
-    
-        public static void main(String[]args){
-Type3 obj = new Type3();
-obj.addvalue(8,9);
-obj.addvalue(5,7);
-        }
-    }
 
