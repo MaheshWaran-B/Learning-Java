bf1ee272642df2358868cacf6703c1535d665228
maheshwaran <152316758+MaheshWaran-B@users.noreply.github.com>
2026-09-16 09:28:48 +0530

    Remove Type1 class and its addvalue method

--- Methods/Type1.java
+++ Methods/Type1.java
@@ -1,14 +1,1 @@
-package Methods;
-// with return type & without arguments
-public class Type1 {
-    public int addvalue(){
-        int a = 9; int b = 10;
-        int sum = a+b;
-        return sum;
 
-    }
-    public static void main(String[]args){
-    Type1 obj = new Type1();
-    int result = obj.addvalue();
-    System.out.println(result);
-}}
